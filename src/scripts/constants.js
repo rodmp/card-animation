58967e11e368c3b9f2df9006f8716e8e7cbea4bd
rodmp <rodomiranda2020@gmail.com>
2020-12-11 08:38:05 -0800

    Add card open

---
+++ src/scripts/constants.js
@@ -0,0 +1,36 @@
+export const LEAN_DEG = -8.8;
+
+/**
+ * Init Scale Animation
+ */
+export const INIT_SCALE = 0.2;
+export const INIT_SCALE_ANI_DURATION = 5000;
+export const INIT_SCALE_ANI_EASING_TYPE = 'easeInOutBounce';
+
+/**
+ * z-index values
+ */
+export const CARD_Z_INDEX_AFTER = 10000;
+export const CARD_Z_INDEX_BEFORE = 1000;
+
+export const COVER_Z_INDEX_AFTER = 995;
+export const COVER_Z_INDEX_BEFORE = 1005;
+
+/**
+ * Init Card Scale
+ */
+export const INIT_CARD_SCALE_X = 0.97;
+export const INIT_CARD_SCALE_Y = 0.92;
+
+/**
+ * Open Cover Animation
+ */
+export const OPEN_COVER_ANI_DURATION = 5000;
+export const OPEN_COVER_ANI_EASING_TYPE = 'easeInOutBounce';
+export const OPEN_COVER_ANI_DELAY = 2000;
+
+/**
+ * Open Card Animation
+ */
+export const OPEN_CARD_ANI_DURATION = 5000;
+export const OPEN_CARD_ANI_EASING_TYPE = 'easeInOutSine';
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,86 @@
 import '../styles/index.scss';
 import anime from 'animejs';
+import * as Config from './constants';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-(function () {
-  var scaleEnv = anime({
-    targets: '.env-wrapper',
-    scale: [
-      { value: 0.2 },
-      { value: 1, duration: 5000, easing: 'easeInOutBounce' },
-    ],
-    rotate: '-8.8',
-    easing: 'linear',
-  });
+const ENV_WRAPPER = '.env-wrapper';
+const ENV_COVER = '.env-cover';
+const ENV_CARD = '.env-card';
 
-  var openCover = anime({
-    targets: '.env-cover',
-    rotateY: '180',
-    duration: 5000,
-    delay: 2000,
-    easing: 'easeInOutBounce',
-  });
+/**
+ * Scale Envelope Animation
+ */
+const scaleAni = anime({
+  targets: ENV_WRAPPER,
+  scale: [
+    { value: Config.INIT_SCALE },
+    {
+      value: 1,
+      duration: Config.INIT_SCALE_ANI_DURATION,
+      easing: Config.INIT_SCALE_ANI_EASING_TYPE,
+    },
+  ],
+  rotate: Config.LEAN_DEG,
+  autoplay: false,
+});
+
+/**
+ * Open Envelope Cover Animation
+ */
+const openCoverAni = anime({
+  targets: ENV_COVER,
+  rotateY: '180',
+  duration: Config.OPEN_COVER_ANI_DURATION,
+  delay: Config.OPEN_COVER_ANI_DELAY,
+  easing: Config.OPEN_COVER_ANI_EASING_TYPE,
+  autoplay: false,
+});
+
+/**
+ * Open Card Animation
+ */
+const openCardAnimation = anime({
+  targets: ENV_CARD,
+  translateX: {
+    value: function (el) {
+      return el.offsetWidth / Config.INIT_CARD_SCALE_X;
+    },
+  },
+  duration: Config.OPEN_CARD_ANI_DURATION,
+  direction: 'alternate',
+  easing: Config.OPEN_CARD_ANI_EASING_TYPE,
+  autoplay: false,
+});
+
+const init = () => {
+  document.querySelector(ENV_CARD).style['z-index'] =
+    Config.CARD_Z_INDEX_BEFORE;
+
+  document.querySelector(ENV_COVER).style['z-index'] =
+    Config.COVER_Z_INDEX_BEFORE;
+};
+
+const scaleAndOpenCoverAni = () => {
+  scaleAni.restart();
+  openCoverAni.restart();
+
+  return Promise.all([scaleAni.finished, openCoverAni.finished]);
+};
+
+const openCardAni = () => {
+  openCardAnimation.restart();
+  return Promise.all([openCardAnimation.finished]);
+};
+
+(async function () {
+  await init();
+  await scaleAndOpenCoverAni();
+  //Change z-index of Envelope Cover
+  document.querySelector(ENV_COVER).style['z-index'] =
+    Config.COVER_Z_INDEX_AFTER;
+
+  await openCardAni();
 }.call(this));
